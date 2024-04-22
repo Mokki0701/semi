10ad6ca9c29fb445f18a630eaab58e210871bc2a
SEUONG WOO <andymon@naver.com>
2024-04-22 09:36:48 +0900

    프로젝트 생성

---
+++ GoWithProject/src/main/java/edu/kh/gowith/GoWithProjectApplication.java
@@ -0,0 +1,14 @@
+package edu.kh.gowith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class GoWithProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoWithProjectApplication.class, args);
+	}
+
+}
---
+++ GoWithProject/src/test/java/edu/kh/gowith/GoWithProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kh.gowith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoWithProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
